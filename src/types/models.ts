5ca98b972be246e91150218063709497bab5e998
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-08 17:14:48 +0300

    chore: model file created

---
+++ src/types/models.ts
@@ -0,0 +1,77 @@
+export type UUID = string;
+
+export interface User {
+  id: UUID;
+  email: string;
+  full_name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Task {
+  id: UUID;
+  user_id: UUID;
+  name: string;
+  priority: "High" | "medium" | "low";
+  status: "Pending" | "onProgress" | "completed";
+  created_at: string;
+  due_date?: string;
+}
+
+export interface Finance {
+  id: UUID;
+  user_id: UUID;
+  amount: number;
+  type: "expense" | "income";
+  reason: string;
+  date: string;
+  payment_method: "cash" | "bank";
+  remaining_balance: number;
+}
+
+export interface MonthlyExpense {
+  id: UUID;
+  user_id: UUID;
+  month: string;
+  year: number;
+  total_expense: number;
+  total_income: number;
+  net_savings: number;
+}
+
+export interface Idea {
+  id: UUID;
+  user_id: UUID;
+  title: string;
+  description: string;
+  created_at: string;
+  tags: string[];
+}
+
+export interface Goal {
+  id: UUID;
+  user_id: UUID;
+  category: "skill" | "project" | "finance" | "personal";
+  title: string;
+  description: string;
+  start_date: string;
+  end_date: string;
+  status: "notStarted" | "onProgress" | "completed";
+}
+
+export interface PersonalNote {
+  id: UUID;
+  user_id: UUID;
+  title?: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface UserProfile {
+  user_id: UUID;
+  bio: string;
+  avatar_url: string;
+  created_at: string;
+  updated_at: string;
+}
