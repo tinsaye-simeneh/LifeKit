23866fe8fa951b5cc506b52056a63eaa3d29ce71
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-08 18:04:38 +0300

    feat: Add MantineProvider to layout.tsx and Button component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-export default function Home() {
+import { Button } from "@mantine/core";
+
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Life kit
-    </div>
+    <Button color="blue" radius="md" size="md">
+      Click Me
+    </Button>
   );
-}
+};
+
+export default Home;
