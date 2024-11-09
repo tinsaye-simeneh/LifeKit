b41571a5e860d993755d3d23918e57be0a3b9839
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-09 13:57:18 +0300

    feat: Refactor layout component to include session management and user greeting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import "@mantine/core/styles.css";
+// app/layout.tsx
+"use client";
 
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { useEffect } from "react";
+import { useSessionStore } from "../store/sessionStore";
 
-export const metadata = {
-  title: "Life kit",
-  description: "A collection of tools for life",
+const Layout = ({ children }) => {
+  const { session, fetchSession } = useSessionStore();
+
+  useEffect(() => {
+    fetchSession();
+  }, [fetchSession]);
+
+  return (
+    <div>
+      {session ? (
+        <p>Welcome, {session.user.email}</p>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+      {children}
+    </div>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider>{children}</MantineProvider>
-      </body>
-    </html>
-  );
-}
+export default Layout;
