d190453e7a21055739a370d5a02ad7b69a079b13
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-13 14:04:12 +0300

    fix: update import path for session store in profile page component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
-import { useSessionStore } from "../path/to/sessionStore";
+import { useSessionStore } from "@/store/sessionStore";
 
 const ProfilePage: React.FC = () => {
   const { session, fetchSession } = useSessionStore();
