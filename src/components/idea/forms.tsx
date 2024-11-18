d8999da70f606513419dd65331ed106581396371
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-18 21:05:38 +0300

    fix: idea UI

--- src/components/idea/forms.tsx
+++ src/components/idea/forms.tsx
@@ -20,14 +20,12 @@ const IdeaForm = ({ initialValues, onSubmit }: IdeaFormProps) => {
   });
 
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-6 bg-gray-50 rounded-lg shadow-lg">
-      <h1 className="text-3xl font-bold text-black mb-4 text-center">
-        {initialValues ? "Create Idea" : "Edit Idea"}
-      </h1>
+    <div className="max-w-4xl mx-auto p-6 space-y-6 bg-gray-50 rounded-lg shadow-lg mt-14">
+      <h1 className="text-3xl font-bold text-black text-center">Idea</h1>
 
       <form
         onSubmit={form.onSubmit(onSubmit)}
-        className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white p-6 rounded-lg shadow-md"
+        className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white p-6 rounded-lg"
       >
         <TextInput
           label="Idea Title"
@@ -47,9 +45,15 @@ const IdeaForm = ({ initialValues, onSubmit }: IdeaFormProps) => {
 
         <Button
           type="submit"
-          className="bg-blue-500 hover:bg-gray-600 text-white ml-auto mt-4 "
+          className="bg-blue-500 hover:bg-gray-600 text-white mt-4 col-span-1"
         >
-          {initialValues ? "Create Idea" : "Edit Idea"}
+          Submit
+        </Button>
+        <Button
+          onClick={() => window.open("/ideas", "_self")}
+          className="w-full col-span-1 bg-red-500 hover:bg-red-600 text-white mt-4"
+        >
+          Cancel
         </Button>
       </form>
     </div>
