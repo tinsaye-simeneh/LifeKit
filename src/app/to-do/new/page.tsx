f66ca90a382b66a2814e53ca3cf804a1b8a0e38b
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-28 22:57:20 +0300

    fix: todo form ui and component

--- src/app/to-do/new/page.tsx
+++ src/app/to-do/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { notifications } from "@mantine/notifications";
-import ToDoForm from "@/components/to-do/forms";
+import TaskForm from "@/components/to-do/forms";
 import { useSessionStore } from "@/store/sessionStore";
 import { useTaskStore } from "@/store/todoStore";
 
@@ -44,20 +44,15 @@ const ToDoPage = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 space-y-6 bg-gray-50 rounded-lg shadow-lg my-20">
-      <h1 className="text-3xl font-bold text-black mb-4 text-center">
-        Create a New Task
-      </h1>
-      <ToDoForm
-        initialValues={{
-          name: "",
-          priority: "low",
-          due_date: new Date().toISOString().split("T")[0],
-          status: "pending",
-        }}
-        onSubmit={handleCreate}
-      />
-    </div>
+    <TaskForm
+      initialValues={{
+        name: "",
+        priority: "low",
+        due_date: new Date().toISOString().split("T")[0],
+        status: "pending",
+      }}
+      onSubmit={handleCreate}
+    />
   );
 };
 
