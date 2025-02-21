e4e483ed9ccbb44ffe6064606a2470863298a653
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2025-02-21 23:59:18 +0300

    refactor: Add Google Calendar event creation to TaskForm submission

--- src/components/to-do/forms.tsx
+++ src/components/to-do/forms.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { useState } from "react";
 import { Button, Select, Textarea, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import RichTextInput from "@/components/RichTextInput";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import { createGoogleCalendarEvent } from "@/utils/googleCalendar";
+import { useAuth } from "@/context/AuthContext";
 
 interface TaskFormProps {
   initialValues: {
@@ -30,6 +34,21 @@ const TaskForm = ({ initialValues, onSubmit }: TaskFormProps) => {
     initialValues,
   });
 
+  const { session } = useAuth();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleFormSubmit = async (values: any) => {
+    setLoading(true);
+
+    await onSubmit(values);
+
+    if (session?.access_token) {
+      await createGoogleCalendarEvent(session.access_token, values);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <ProtectedRoute>
       <div className="max-w-4xl mx-auto p-6 space-y-6 bg-gray-50 rounded-lg shadow-lg my-20">
@@ -38,11 +57,7 @@ const TaskForm = ({ initialValues, onSubmit }: TaskFormProps) => {
         </h1>
 
         <form
-          onSubmit={form.onSubmit(async (values) => {
-            setLoading(true);
-            await onSubmit(values);
-            setLoading(false);
-          })}
+          onSubmit={form.onSubmit(handleFormSubmit)}
           className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-white p-6"
         >
           <Textarea
