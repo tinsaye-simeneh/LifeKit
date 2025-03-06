9ee5fef20b30fae11aea1091f953d3e7001e466e
Tinsaye S. Dessie <64516393+tinsaye-simeneh@users.noreply.github.com>
2025-03-06 11:19:13 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,21 +6,7 @@ export default function Footer() {
     <footer className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 text-white py-6 border-t-2 border-gray-400">
       <Container size="lg" className="text-center">
         <p className="mt-5">
-          &copy; {currentYear} LifeKit. Made with ❤️ by
-          <a
-            href="https://github.com/HikmaAnwar"
-            className="text-blue-500 underline"
-          >
-            {" "}
-            Hikma
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://github.com/tinsaye-simeneh"
-            className="text-blue-500 underline"
-          >
-            Tinsaye
-          </a>
+          &copy; {currentYear} LifeKit. All rights reserved.
         </p>
       </Container>
     </footer>
